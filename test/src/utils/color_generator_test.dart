62a3076a6335dc6022cca4f50198fe76f366b6e5
Orangunio <miloszdubowski@wp.pl>
2026-04-01 21:51:02 +0200

    Deleting warnings in color_generator_test

--- test/src/utils/color_generator_test.dart
+++ test/src/utils/color_generator_test.dart
@@ -6,33 +6,53 @@ void main() {
   const int expectedOpacity = 255;
   const int maxRGB = 255;
   const int minRGB = 0;
+  const int alphaShift = 24;
+  const int redShift = 16;
+  const int greenShift = 8;
+  const int byteMask = 0xFF;
+
+  const int sampleSize = 10;
+  const int minUniqueCount = 1;
+
+  const int channelsPerComponent = 256;
+  const int expectedTotalColors = 16777216;
+
   group('ColorGenerator', () {
     test('nextColor returns a fully opaque Color', () {
       final Color color = ColorGenerator.generateRandomColor();
-      expect((color.toARGB32() >> 24) & 0xFF, equals(expectedOpacity));
+      
+      final int alpha = (color.toARGB32() >> alphaShift) & byteMask;
+      expect(alpha, equals(expectedOpacity));
     });
 
     test('nextColor RGB channels are within 0-255', () {
       final Color color = ColorGenerator.generateRandomColor();
-      expect((color.toARGB32() >> 16) & 0xFF, inInclusiveRange(minRGB, maxRGB));
-      expect((color.toARGB32() >> 8) & 0xFF, inInclusiveRange(minRGB, maxRGB));
-      expect(color.toARGB32() & 0xFF, inInclusiveRange(minRGB, maxRGB));
+      final int argb = color.toARGB32();
+
+      final int red = (argb >> redShift) & byteMask;
+      final int green = (argb >> greenShift) & byteMask;
+      final int blue = argb & byteMask;
+
+      expect(red, inInclusiveRange(minRGB, maxRGB));
+      expect(green, inInclusiveRange(minRGB, maxRGB));
+      expect(blue, inInclusiveRange(minRGB, maxRGB));
     });
 
     test('successive calls produce different colors with high probability', () {
-      final colors = List.generate(
-        10,
+      final List<Color> colors = List.generate(
+        sampleSize,
         (_) => ColorGenerator.generateRandomColor(),
       );
-      final uniqueCount = colors.toSet().length;
-      expect(uniqueCount, greaterThan(1));
+      final int uniqueCount = colors.toSet().length;
+      
+      expect(uniqueCount, greaterThan(minUniqueCount));
     });
 
     test('can generate all 16,777,216 possible RGB values', () {
-      const int channelsPerComponent = 256;
       const int totalColors =
           channelsPerComponent * channelsPerComponent * channelsPerComponent;
-      expect(totalColors, equals(16777216));
+          
+      expect(totalColors, equals(expectedTotalColors));
     });
   });
 }
